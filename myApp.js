8ba9fdf039138ad26388b5e2f0c704c31151d067
María Luisa Martínez <mluisamartinezc@gmail.com>
2022-02-14 11:08:24 +0100

    Use model.findOne()

--- myApp.js
+++ myApp.js
@@ -36,7 +36,10 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food}, function (err, person) {
+    if (err) return console.log(err);
+    done(null, person);
+  });
 };
 
 const findPersonById = (personId, done) => {
