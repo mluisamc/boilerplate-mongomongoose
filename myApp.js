70aa466f5051497a12f8c9a1638979d460ffe7a2
María Luisa Martínez <mluisamartinezc@gmail.com>
2022-02-14 10:46:46 +0100

    Create a Model

--- myApp.js
+++ myApp.js
@@ -3,7 +3,14 @@ var mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const { Schema } = mongoose;
+const personSchema = new Schema({
+  name:  String,
+  age: Number,
+  favoriteFoods: [String]
+});
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
