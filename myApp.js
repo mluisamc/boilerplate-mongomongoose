8c88b6a97fec433bdd82d6a27c0dc079a04fae56
María Luisa Martínez <mluisamartinezc@gmail.com>
2022-02-14 11:41:35 +0100

    fix query

--- myApp.js
+++ myApp.js
@@ -91,7 +91,7 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  Person.find({favoriteFoods: foodToSearch}).sort('name').limit(2).select('name','favoriteFoods').exec((err, query) => {
+  Person.find({favoriteFoods: foodToSearch}).sort({ name: 1 }).limit(2).select({ name: 1, age: 0, favoriteFoods: 1 }).exec((err, query) => {
     if(err) return console.log(err);
     done(null, query);
   })
